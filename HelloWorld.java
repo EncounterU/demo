348980a3c54998cfc59e2bbfb150ca91df539b94
EncounterU <914935621@qq.com>
2020-03-17 17:42:57 +0800

    helloworld

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+       
+        System.out.println("hi!");
+        System.out.println(one.price);
+        System.out.println(one.color);
+        System.out.println("==================");
+       
+    }
